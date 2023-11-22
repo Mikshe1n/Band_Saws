2d580b757ac633ba7b9e9b132a2138a0a380e389
Mikshe1n <Mishaili4ev@yandex.ru>
2023-11-22 22:18:27 +0300

    verst-site-ready

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -8,27 +8,62 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('.scroll-section');
 
+    function activateSection() {
+        let scrollPosition = window.scrollY;
 
-let burger = document.getElementById("pila");
-let name = document.getElementById("name");
-let phone = document.getElementById("phone");
-document .getElementById("order-action").onclick = function () {
-    let hasError = false;
+        sections.forEach(section => {
+            const offset = section.offsetTop - window.innerHeight / 2;
+            const height = section.offsetHeight;
+            const id = section.getAttribute('id');
 
-    [burger, name, phone].forEach (item => {
-        if (!item.value) {
-            item.parentElement.style.background = "red";
-            hasError = true;
-        } else {
-            item.parentElement.style.background = "";
-        }
-    })
-
-    if (!hasError) {
-        [burger, name, phone].forEach (item => {
-           item.value = "";
-        }); 
-        alert ("Спасибо за заказ! Мы скоро свяжемся с вами!");
+            if (scrollPosition >= offset && scrollPosition < offset + height) {
+                removeAllActiveClasses();
+                section.classList.add('active');
+            }
+        });
     }
+
+    function removeAllActiveClasses() {
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+    }
+
+    window.addEventListener('scroll', activateSection);
+
+    // Call the function on page load to highlight the initial section
+    activateSection();
+});
+
+
+
+
+
+document.getElementById('order-action').addEventListener('click', function () {
+    validateForm();
+});
+
+function validateForm() {
+    var pila = document.getElementById('pila').value;
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+
+    if (pila === '' || name === '' || phone === '') {
+        alert('Пожалуйста, заполните все поля формы.');
+        return false;
+    }
+
+    // Дополнительная валидация телефона (можно использовать регулярные выражения)
+    var phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phone)) {
+        alert('Пожалуйста, введите корректный номер телефона.');
+        return false;
+    }
+
+    // Если все поля заполнены и валидны, можно отправить форму на сервер или выполнить другие действия.
+    alert('Ваш заказ успешно оформлен!');
+    // Дополнительные действия, например, отправка данных на сервер
 }
