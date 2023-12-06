809670cc253f62bcb8605ec2c9dde361aaffdcbb
Mikshe1n <Mishaili4ev@yandex.ru>
2023-12-06 16:09:07 +0300

    verst-site-r

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -40,30 +40,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-
-
-document.getElementById('order-action').addEventListener('click', function () {
-    validateForm();
-});
-
-function validateForm() {
-    var pila = document.getElementById('pila').value;
-    var name = document.getElementById('name').value;
-    var phone = document.getElementById('phone').value;
-
-    if (pila === '' || name === '' || phone === '') {
-        alert('Пожалуйста, заполните все поля формы.');
-        return false;
-    }
-
-    // Дополнительная валидация телефона (можно использовать регулярные выражения)
-    var phoneRegex = /^\d{10}$/;
-    if (!phoneRegex.test(phone)) {
-        alert('Пожалуйста, введите корректный номер телефона.');
-        return false;
-    }
-
-    // Если все поля заполнены и валидны, можно отправить форму на сервер или выполнить другие действия.
-    alert('Ваш заказ успешно оформлен!');
-    // Дополнительные действия, например, отправка данных на сервер
-}
